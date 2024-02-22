72b114300bc4ccd90b1c90b1d11f24b7f3ff9de1
hubert22143 <33151182+hubert22143@users.noreply.github.com>
2024-02-23 00:42:42 +0100

    dropDownMenuLogic

--- Drop-Down menu-2/src/dropDownMenuLogic.js
+++ Drop-Down menu-2/src/dropDownMenuLogic.js
@@ -1,33 +1,42 @@
-let rotationAngle = 0;
 let initialCounter = 0;
-
 export function initializeDropDownMenu() {
   const menus = document.querySelectorAll(".menu");
   menus.forEach((menu) => {
     const selectItem = menu.querySelector(".menuWrapper");
-    const icon = selectItem.querySelector(".hIcon");
-
-    if (!selectItem.hasEventListener) {
-      selectItem.addEventListener("click", () => {
-        rotateIcon(icon);
-        showMenuList(menu);
+    const menuList = menu.querySelector(".menuList");
+    const liElements = menu.querySelectorAll(".ulElements li");
+    const menuHeader = menu.querySelector(".selectedItem");
+    const menuWrapper = menu.querySelector(".menuWrapper");
+    const hIcon = menuWrapper.querySelector(".hIcon");
+    selectItem.addEventListener("click", () => {
+      showMenuList(menuList);
+      if (initialCounter % 2 === 0) {
+        hIcon.classList.remove("active");
+      } else {
+        hIcon.classList.add("active");
+      }
+    });
+    liElements.forEach((element) => {
+      element.addEventListener("click", (event) => {
+        activateElement(element, menuHeader, menuList);
       });
-      selectItem.hasEventListener = true;
-    }
+    });
   });
 }
-
-function rotateIcon(icon) {
-  rotationAngle += 180;
-  icon.style.transform = `rotate(${rotationAngle}deg)`;
+function showMenuList(menuList) {
+  initialCounter++;
+  const isActive = initialCounter % 2 === 1;
+  menuList.style.opacity = isActive ? 1 : 0;
+  menuList.classList.toggle("active", isActive);
 }
 
-function showMenuList(menu) {
-  const menuList = menu.querySelector(".menuList");
+function activateElement(element, menuHeader, menuList) {
+  const hIcon = document.querySelector(".hIcon");
+  const liElements = element.parentElement.querySelectorAll("li");
+  hIcon.classList.remove("active");
+  liElements.forEach((el) => el.classList.remove("active"));
+  menuHeader.textContent = element.textContent;
+  element.classList.add("active");
+  menuList.style.opacity = 0;
   initialCounter++;
-  if (initialCounter % 2 === 0) {
-    menuList.style.opacity = 0;
-  } else {
-    menuList.style.opacity = 1;
-  }
 }
